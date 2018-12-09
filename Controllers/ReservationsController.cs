9b82b61b8414a1a9879b2093e964fd6a0138556c
demir005 <demirmah93@gmail.com>
2018-12-09 18:32:18 +0100

    Expresso Web Api
    Expresso Web Api using ASP.NET Entity Framework

---
+++ Controllers/ReservationsController.cs
@@ -0,0 +1,27 @@
+﻿using ExpressoWebApi.Data;
+using ExpressoWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExpressoWebApi.Controllers
+{
+    public class ReservationsController : ApiController
+    {
+        ExpressoDbContext expressoDbContex = new ExpressoDbContext();
+
+        public IHttpActionResult Post(Reservation reservation)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            expressoDbContex.Reservations.Add(reservation);
+            expressoDbContex.SaveChanges();
+            return StatusCode(HttpStatusCode.Created);
+        }
+    }
+}
---
+++ Data/ExpressoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ExpressoWebApi.Models;
+using System.Data.Entity;
+
+
+namespace ExpressoWebApi.Data
+{
+    public class ExpressoDbContext : DbContext
+    {
+        public DbSet<Menu> Menus { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+
+
+    }
+}
